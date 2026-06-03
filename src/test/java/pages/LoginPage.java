95a15317929adef8a5e39761db60edb5ae051120
Mansoor-Pasha <mansoorpasha917@gmail.com>
2026-06-03 16:15:12 +0530

    commiting the required documents

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	
+	private WebDriver driver;
+	
+	private By usernameField  = By.xpath("//input[@name = 'username']");
+}
---
+++ src/test/java/utils/Base.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() 
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+	
+	@AfterMethod
+	public void tearDown ()
+	{
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+}
