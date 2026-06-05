fd0c4a779d6ae4de8dac275c5a50e05a59ed10d0
Mansoor-Pasha <mansoorpasha917@gmail.com>
2026-06-05 22:52:13 +0530

    Added TestCases to DashBoardTest.java File

--- src/test/java/pages/DashBoardPage.java
+++ src/test/java/pages/DashBoardPage.java
@@ -5,39 +5,88 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DashBoardPage {
 	
 	WebDriver driver;
-	
-	By selfReview = By.xpath("//p[text() = '(1) Pending Self Review']");
-	By mainMenuButton = By.xpath("//button[@class='oxd-icon-button oxd-main-menu-button']");
-	By upgradeButton = By.xpath("//button[@class='oxd-glass-button orangehrm-upgrade-button']");
-	By searchPlaceHolder = By.xpath("//input[@class='oxd-input oxd-input--active']");
-	By adminSection = By.xpath("//span[text()='Admin']");
-	By pimSection = By.xpath("//span[text()='PIM']");
-	By leaveSection = By.xpath("//span[text()='Leave']");
-	By timeSection = By.xpath("//span[text()='Time']");
-	By performanceSection = By.xpath("//span[text()='Performance']");
-
-	
-	
-	private WebDriverWait wait;
-	
-	public String getCurrentURL ()
-	{
-		return driver.getCurrentUrl();
-	}
+	WebDriverWait wait;
 	
 	public DashBoardPage (WebDriver driver) {
 		this.driver = driver;
 		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 	
+	// For Selecting the Country Name from DropDown of UPGRADE PAGE
+	public void countrySelection ()
+	{
+		WebElement dropDownElement = driver.findElement(By.id("Form_getForm_Country"));
+		Select selectCountry = new Select (dropDownElement);
+		selectCountry.selectByValue("India");
+	}
+	
+	//For Selecting the Number of Employees from UPGRADE PAGE DropDown
+	public void NumberOfEmployeeSelect ()
+	{
+		WebElement dropDownElement = driver.findElement(By.id("Form_getForm_NoOfEmployees"));
+		Select selectEmployee = new Select (dropDownElement);
+		selectEmployee.selectByValue("11 - 50");
+	}
+	
+	
+	// DashBoard Header Paths 
+	By dashboardText = By.xpath("//h6[text() = 'Dashboard']");
+	By upgradeButton = By.xpath("//button[@class='oxd-glass-button orangehrm-upgrade-button']");
+	
+	//UpgradePage Information
+	public void urlUpgradePage ()
+	{
+		driver.get("https://orangehrm.com/open-source/upgrade-to-advanced");
+	}
+	
+	By UpgradeFullName = By.xpath("//input[@id= 'Form_getForm_FullName']");
+	By UpgradeEmail = By.xpath("//input[@id= 'Form_getForm_Email']");
+	By UpgradePhoneNumber = By.xpath("//input[@id= 'Form_getForm_Contact']");
+	By UpgradeYourFullName = By.xpath("//input[@id= 'Form_getForm_CompanyName']");
+	By UpgradeJobTitle = By.xpath("//input[@id= 'Form_getForm_JobTitle']");
+	By UpgradeNumOfEmployees = By.xpath("//div[@class= 'recaptcha-checkbox-borderAnimation']");
+	By UpgradeSubmit = By.xpath("//input[@class= 'action']");
+	
+	//Search Box Path
+	By searchPlaceHolder = By.xpath("//input[@class='oxd-input oxd-input--active']");
+	
+	//Left Menu Paths	
+	By adminSection = By.xpath("//span[text()='Admin']");
+	By pimSection = By.xpath("//span[text()='PIM']");
+	By leaveSection = By.xpath("//span[text()='Leave']");
+	By timeSection = By.xpath("//span[text()='Time']");
+	By recruitmentSection = By.xpath("//span[text()='Recruitment']");
+	By myInfoSection = By.xpath("//span[text()='My Info']");
+	By performanceSection = By.xpath("//span[text()='Performance']");
+	By directorySection = By.xpath("//span[text()='Directory']");
+	By maintainSection = By.xpath("//span[text()='Maintenance']");
+	By claimSection = By.xpath("//span[text()='Claim']");
+	By buzzSection = By.xpath("//span[text()='Buzz']");
+	
+	//Profile Paths
+	By profileDropDown = By.xpath("//ul[@class='oxd-dropdown-menu']");
+	By logout = By.xpath("//a[text()='Logout']");
+	
+	// To get CurrentURL of the Page
+	public String getCurrentURL ()
+	{
+		return driver.getCurrentUrl();
+	}
+	
+	//Method for Getting DashBoardText of the Dashboard
+	public String getDashboardText ()
+	{
+		return driver.findElement(dashboardText).getText();
+	}
+	
 	//Method for Navigating to Upgrade Page
-	public void upgradePage ()
+	public void clickUpgradePage ()
 	{
 		driver.findElement(upgradeButton).click();
 	}
@@ -49,74 +98,82 @@ public class DashBoardPage {
 	}
 	
 	//Method for Search Place Holder 
-	public void searchPlace ()
+	public void searchPlace (String menuName)
 	{
-		driver.findElement(searchPlaceHolder);
-	}
-	
-	//Method for getting Search Place URL
-	public String searchPageURL ()
-	{
-		return driver.getCurrentUrl();
+		driver.findElement(searchPlaceHolder).clear();
+		driver.findElement(searchPlaceHolder).sendKeys(menuName);
 	}
 	
 	//Method for Navigating to Admin Section
-	public void adminSection ()
+	public void clickAdmin ()
 	{
-		driver.findElement(adminSection);
-	}
-	
-	//Method for getting Admin Section URL
-	public String adminPageURL ()
-	{
-		return driver.getCurrentUrl();
+		driver.findElement(adminSection).click();
 	}
 	
 	//Method for Navigating to PIM Section
-	public void pimSection ()
+	public void clickPIM ()
 	{
-		driver.findElement(pimSection);
-	}
-	
-	//Method for getting PIM Section Page URL
-	public String pimPageURL ()
-	{
-		return driver.getCurrentUrl();
+		driver.findElement(pimSection).click();
 	}
 	
 	//Method for Navigating to Leave Section
-	public void leaveSection ()
+	public void clickLeave ()
 	{
-		driver.findElement(leaveSection);
-	}
-	
-	//Method for getting Leave Section URL
-	public String leavePageURL ()
-	{
-		return driver.getCurrentUrl();
+		driver.findElement(leaveSection).click();
 	}
 	
 	//Method for Navigating to Time Section
-	public void timeSection ()
+	public void clickTime ()
 	{
-		driver.findElement(timeSection);
+		driver.findElement(timeSection).click();
 	}
 	
-	//Method for getting Time Section URL
-	public String timePageURL ()
+	//Method for Navigating to Recruitment Section
+	public void clickRecruitment ()
 	{
-		return driver.getCurrentUrl();
+		driver.findElement(recruitmentSection).click();
+	}
+	
+	//Method for Navigating to MyInfo Section
+	public void clickMyInfo ()
+	{
+		driver.findElement(myInfoSection).click();
 	}
 	
 	//Method for Navigating to Performance Section
-	public void performanceSection ()
+	public void clickPerformance ()
 	{
-		driver.findElement(performanceSection);
+		driver.findElement(performanceSection).click();
 	}
 	
-	//Method for getting Performance Section URL
-	public String performancePageURL ()
+	//Method for Navigating to Directory Section
+	public void clickDirectory ()
 	{
-		return driver.getCurrentUrl();
-	}	
+		driver.findElement(directorySection).click();
+	}
+	
+	//Method for Navigating to Maintainance Section
+	public void clickMaintainance ()
+	{
+		driver.findElement(maintainSection).click();
+	}
+	
+	//Method for Navigating to Claim Section
+	public void clickClaim ()
+	{
+		driver.findElement(claimSection).click();
+	}
+	
+	//Method for Navigating to Buzz Section
+	public void clickBuzz ()
+	{
+		driver.findElement(buzzSection).click();
+	}
+	
+	//Method for Logging Out 
+	public void logout ()
+	{
+		driver.findElement(profileDropDown).click();
+		driver.findElement(logout).click();
+	}
 }
--- src/test/java/tests/DashBoardTest.java
+++ src/test/java/tests/DashBoardTest.java
@@ -1,8 +1,58 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.DashBoardPage;
 import utils.BaseTest;
 
 public class DashBoardTest extends BaseTest  {
+
+	/**
+	 * -: TEST CASES :- 
+	 * TC_001 :- Verifying the visibility of Dashboard text in dashboard
+	 * TC_002 :- Verifying the Upgrade button is Clickable or not in Dashboard page
+	 * TC_003 :- Verifying the Search Menu 
+	 * TC_004 :- Verifying the Visibility of the "admin" text in the Admin page URL
+	 * TC_005 :- Verifying the Visibility of the "pim" text in the PIM page URL
+	 * TC_006 :- Verifying the Visibility of the "leave" text in the Leave page URL
+	 * TC_007 :- Verifying the Visibility of the "time" text in the Time page URL
+	 * TC_008 :- Verifying the Visibility of the "recruitement" text in the Recruitement page URL
+	 * TC_009 :- Verifying the Visibility of the "viewpersonal" text in the MyInfo page URL
+	 * TC_010 :- Verifying the Visibility of the "performance" text in the Performance page URL
+	 * TC_011 :- Verifying the Visibility of the "directory" text in the Directory page URL
+	 * TC_012 :- Verifying the Visibility of the "maintainance" text in the Maintainance page URL
+	 * TC_013 :- Verifying the Visibility of the "claim" text in the Claim page URL
+	 * TC_014 :- Verifying the Visibility of the "buzz" text in the Buzz page URL 
+	*/
 	
+	@Test (priority = 1)
+	public void testDashBoardText ()
+	{
+		DashBoardPage db = new DashBoardPage(driver);		
+		Assert.assertEquals(db.getDashboardText(), 
+				"Dashboard");
+	}
+	
+	@Test (priority = 2)
+	public void testClickUpgradeButton ()
+	{
+		DashBoardPage db = new DashBoardPage(driver);
+		db.clickUpgradePage();
+		
+		String currentURL = db.getCurrentURL();
+		Assert.assertTrue(currentURL.contains("upgrade-to-advanced"));
+	}
+	
+	@Test (priority = 3, dependsOnMethods = "testClickUpgradeButton")
+	public void testUpgradeInfo ()
+	{
+		DashBoardPage db = new DashBoardPage(driver);
+		db.urlUpgradePage();
+		
+		db.countrySelection();
+		db.NumberOfEmployeeSelect();
+		
+	}
 	
 }
